aa6a2a24e471805214bfc3942848a5139baeed18
Babu Nair <babunairofficial@gmail.com>
2024-05-15 19:42:48 +0530

    object prototype

---
+++ Frontend/app.js
@@ -0,0 +1,9 @@
+let arr1 = [1, 2, 3, 4];
+let arr2 = ['a', 'b', 'c'];
+arr1.sayHello = () => {
+    console.log("hello!, i am arr1");
+};
+
+arr2.sayHello = () => {
+    console.log("hello!, i am arr2");
+};
