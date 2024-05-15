1cf1cad8f23a23b725975d783ed07a7b6ed80c41
Babu Nair <babunairofficial@gmail.com>
2024-05-15 14:01:03 +0530

    handling post requests (parse)

--- Backend/index.js
+++ Backend/index.js
@@ -2,14 +2,22 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+app.use(express.urlencoded({extended: true})); //parse
+
 //to accept get requests
 app.get("/register", (req, res) => {
-    res.send("standard GET response");
+    //for get request data comes in req.query
+    let{user, password} = req.query;
+    res.send(`standard GET response, welcome ${user}!`);
 });
 
 //to accept post requests
 app.post("/register", (req, res) => {
-    res.send("standard POST response");
+    // console.log(req.body); 
+    
+    //for post request data comes in req.body
+    let{user, password} = req.body;
+    res.send(`standard POST response, welcome ${user}!`);
 });
 
 app.listen(port, () => {
